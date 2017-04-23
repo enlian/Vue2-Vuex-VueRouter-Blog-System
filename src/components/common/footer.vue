29883af61ca7e775cf0ed37c476a0439d8bbc771
wusong <651754835@qq.com>
2017-04-23 23:24:35 +0800

    update

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer">
     <p>技术栈：Vue2 + Vue-router + Webpack + ElementUI</p>
-    <p>Code by Song 651754835@qq.com</p>
+    <p>Code by Song , 651754835@qq.com</p>
   </div>
 </template>
 
@@ -13,6 +13,6 @@
 
 
 <style scoped>
-.footer{margin-top: 50px;text-align: center;border-top: 1px solid #ececec;padding-top: 20px}
-.footer p{color: #999;font-size: 12px}
+.footer{margin-top: 50px;text-align: center;border-top: 1px solid #eef1f6;padding-top: 20px}
+.footer p{color: #adadad;font-size: 12px}
 </style>
--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="top-nav">
-    <el-menu theme="dark" default-active="/" class="el-menu-demo" mode="horizontal" :router=true>
+    <el-menu default-active="/" mode="horizontal" :router=true>
       <el-menu-item index="/"><i class="fa fa-home"> 首页</i></el-menu-item>
-      <el-submenu index="2" v-if="isLogin">
+      <el-submenu index="" v-if="isLogin" class="pull-right">
         <template slot="title"><i class="fa fa-user"> {{form.userName}}</i></template>
-        <el-menu-item index="2-1"><i class="fa fa-sign-out"> 退出</i></el-menu-item>
+        <el-menu-item index="" @click="logout"><i class="fa fa-sign-out"> 退出</i></el-menu-item>
       </el-submenu>
-
-      <el-button class="login-btn" type="primary" @click="loginDialogVisible = true" v-if="!isLogin"><i class="fa fa-user"> 登录</i>
+      <el-button class="login-btn" type="primary" @click="loginDialogVisible = true" v-if="!isLogin"><i
+        class="fa fa-user"> 登录</i>
       </el-button>
     </el-menu>
 
     <!--登录弹窗-->
-    <el-dialog title="登录" v-model="loginDialogVisible" size="tiny1">
+    <el-dialog title="登录" v-model="loginDialogVisible" size="tiny1" top="15%">
       <el-form :model="form" :inline=true>
         <el-form-item label="用户名：">
           <el-input v-model="form.userName" auto-complete="off" placeholder="无需密码，随便输入" icon="edit"></el-input>
         </el-form-item>
-        <el-button type="primary" @click="login">确 定</el-button>
+        <el-button type="primary" @click="login">登 录</el-button>
       </el-form>
     </el-dialog>
   </div>
@@ -34,14 +34,35 @@
         form: {
           userName: ''
         },
-        loginDialogVisible:false
+        loginDialogVisible: false
       }
     },
     methods: {
       login: function () {
-        this.loginDialogVisible = false
-        this.isLogin = true
-        console.log(this.form.userName)
+        if (this.form.userName == '') {
+          this.$notify.error({
+            title: '错误',
+            message: '请输入用户名！',
+            offset: 80
+          });
+        } else {
+          this.loginDialogVisible = false
+          this.isLogin = true
+          this.$notify.info({
+            title: '消息',
+            message: '登录成功！',
+            offset: 80
+          });
+        }
+      },
+      logout: function () {
+        this.isLogin = false
+        this.form.userName = ''
+        this.$notify.info({
+          title: '消息',
+          message: '登出成功！',
+          offset: 80
+        });
       }
     }
   }
@@ -49,7 +70,10 @@
 
 
 <style scoped>
-  .top-nav{margin-bottom: 20px}
+  .top-nav {
+    margin-bottom: 20px
+  }
+
   a:-webkit-any-link {
     text-decoration: none
   }
