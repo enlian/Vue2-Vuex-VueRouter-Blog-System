298a8c2df04a616a96e18e6ef479a5907e8e3618
enlian <frejaenlian@gmail.com>
2024-10-10 14:59:44 +0900

    fix bug

--- src/components/header.vue
+++ src/components/header.vue
@@ -10,7 +10,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">System</a>
+          <a class="navbar-brand" href="#">Blog</a>
         </div>
 
         <div class="collapse navbar-collapse" id="mobile-nav">
